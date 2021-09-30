32e39ecfd129fc433f0f50cce04024fb8df1336a
Андрей Разумов <alexkirillof@yandex.ru>
2021-09-30 22:22:50 +0300

    Lesson1

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const Items = [
 {id:1,name:'Notebook', img:"img/1.jpg", price:2400},
 {id:2,name:'Mouse', img:"img/2.jpg", price:20},
 {id:3,name:'Keyboard', img:"img/3.jpg", price:230},
-{id:4,name:'Gamepad', img:"img/4.jpg", price:50}
+{id:4,name:'Gamepad', img:"img/4.jpg", price:70}
 ];
 
 const part = item => {
