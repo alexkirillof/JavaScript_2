eb68b79dde5a2ddaae95896f8c9c972187fe77b2
Андрей Разумов <alexkirillof@yandex.ru>
2021-09-30 22:19:14 +0300

    Lesson_1

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const Items = [
+{id:1,name:'Notebook', img:"img/1.jpg", price:2400},
+{id:2,name:'Mouse', img:"img/2.jpg", price:20},
+{id:3,name:'Keyboard', img:"img/3.jpg", price:230},
+{id:4,name:'Gamepad', img:"img/4.jpg", price:50}
+];
+
+const part = item => {
+   return `
+        <div class="product-item">
+        <h3>${item.name}</h3>
+        <img src="${item.img}">
+        <p>${item.price}</p>
+        <button class="buy-btn">Купить</button>
+    </div> 
+    `
+};
+
+const render = list => {
+    const productList = list.map(item=>part(item)).join('');
+    document.querySelector('.products').innerHTML = productList;
+}
+
+render(Items);
+
+
