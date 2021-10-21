3ac50fd5812e207031b7fe96f45446733efbb8ea
Андрей Разумов <alexkirillof@yandex.ru>
2021-10-21 23:25:01 +0300

    Lesson6

--- js/main.js
+++ js/main.js
@@ -22,21 +22,102 @@ const service = (method,postfix) =>(
 
  Vue.component('basket-goods-item',{
     props:['item'],
+    data: function () {
+        return {
+          style: {
+            padding: '30px 15px',
+            display: 'grid',
+            gridTemplateColumns: 'min-content 1fr min-content'
+          }
+        }
+      },
     template:`
-    <div class="basket-goods-item">  
+    <div class="basket-goods-item" :style="style">  
      <div>{{item.title}}</div>
      <img src=item.img alt={{item.img}}>
      <div>{{item.price}}</div>
-    </div>`,
+    </div>`
 })
+
+
 
 Vue.component('basket-card',{
+    data: function () {
+        return {
+          styles: {
+            root: {
+              display: 'grid',
+              gridTemplateRows: 'min-content 1fr min-content'
+            },
+            header: {
+              padding: '20px',
+              background: 'lightgray'
+            },
+            footer: {
+                padding: '20px',
+                background: 'grey'
+              }
+          },
+        }
+      },
     template:`
-    <div class="basket-card">  
-    
-    </div>`,
-})
+        <div class="basket-card" :style="styles.root" >
+         <div :style="styles.header">
+         <slot name="header"></slot>
+         </div>
+         <div>
+         <slot></slot>
+         </div>
+         <div :style="styles.footer">
+         <slot name="footer"></slot>
+         </div>
+    </div>
+  `
+});
 
+Vue.component('custom-button', {
+    data: 
+    function () {
+      return {
+        style: {
+          border: '1px solid grey',
+          padding: '6px',
+          borderRadius: '5px',
+          cursor: 'pointer'
+        }
+      }
+    },
+    template: `
+      <button :style="style" @click="$emit('open')">
+      <slot></slot>
+      </button>
+    `
+  })
+
+  Vue.component('close-button', {
+    data: 
+    function () {
+      return {
+        style: {
+            position: 'absolute',
+            fontWeight: '700',
+            fontSize: '15px',
+            lineHeight: '15px',
+            opacity: '0.6',
+            cursor: 'pointer',
+            top: '19px',
+            right: '19px'
+         
+        }
+      }
+    },
+    template: `    
+      <span :style="style" @click="$emit('close')">✖</span>
+    `
+  })
+
+
+  
 
 Vue.component('goods-item',{
     props:['item'],
@@ -45,7 +126,7 @@ Vue.component('goods-item',{
      <div>{{item.title}}</div>
      <div><img src="" alt=""></div>
      <div>{{item.price}}</div>
-    
+    <custom-button>Add</custom-button>
     </div>`,
 })
 
@@ -56,12 +137,19 @@ const app = new Vue({
      filteredGoods:GOODS,
      basketCardVision:false,
      search:'',
+     classes:['body','border'],
  },
-  methods: {
-      filterGoods: function(event){
+     methods: {
+      filterGoods: function(){
         this.filteredGoods = this.goods.filter(({title})=>{return new RegExp(this.search, 'i').test(title)})
-      }
-  }
+      },
+      openCart: function(){
+          this.basketCardVision = true;
+      },
+      closeCart: function(){
+        this.basketCardVision = false;
+    }
+     }
 })
 
 
